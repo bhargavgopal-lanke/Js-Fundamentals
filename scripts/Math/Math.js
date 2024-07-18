f63968548893ddf491f56b8990646f4b612629e2
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-18 13:43:31 +0530

    Math methods in js

---
+++ scripts/Math/Math.js
@@ -0,0 +1,16 @@
+let x;
+
+// to find the square root of an number
+
+x = Math.sqrt(9); // 3
+
+// absolute return a positive value even if the value is negative
+
+x = Math.abs(-5);
+
+// to get the round figure value of an number use round method
+// based on values below or above 0.5 we will get the round figure value
+
+x = Math.round(4.4);
+
+console.log(x);
