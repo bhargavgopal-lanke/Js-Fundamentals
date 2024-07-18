8114d69f0955ddfa31aef94db3abaccdbf04c31d
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-18 16:34:33 +0530

    comments added.

--- scripts/Math/Math-Challenge.js
+++ scripts/Math/Math-Challenge.js
@@ -9,26 +9,32 @@ x1 = Math.floor(x1);
 let y1 = Math.random() * 50 + 1;
 
 y1 = Math.floor(y1);
+
+// get the sum
 let sum = x1 + y1;
 const sumOutput = `${x1} + ${y1} = ${sum}`;
 
 console.log("sumOutput", sumOutput);
 
+// get the difference
 let substract = x1 - y1;
 const differenceOutput = `${x1} - ${y1} = ${substract}`;
 
 console.log("differenceOutput", differenceOutput);
 
+// get the multiply
 let multiply = x1 * y1;
 const productOutput = `${x1} * ${y1} = ${multiply}`;
 
 console.log("productOutput", productOutput);
 
+// get the divison
 let division = x1 / y1;
 const quotientOutput = `${x1} / ${y1} = ${division}`;
 
 console.log("quotientOutput", quotientOutput);
 
+// get the remainder
 let ramainder = x1 % y1;
 const remainderOutput = `${x1} % ${y1} = ${ramainder}`;
 
