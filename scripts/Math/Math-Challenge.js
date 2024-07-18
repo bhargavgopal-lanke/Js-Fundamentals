d79db61a2672ab04c26d8d38c3fac690c6f31a21
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-18 14:41:46 +0530

    code optimised.

--- scripts/Math/Math-Challenge.js
+++ scripts/Math/Math-Challenge.js
@@ -2,30 +2,30 @@
 
 let x1 = Math.random() * 100 + 1;
 
-console.log("x1", Math.floor(x1));
+x1 = Math.floor(x1);
 
 // create a random number in between 1 and 50
 
 let y1 = Math.random() * 50 + 1;
 
-console.log("y1", Math.floor(y1));
+y1 = Math.floor(y1);
 
 const sumOutput = x1 + y1;
 
-console.log("sumOutput", Math.floor(sumOutput));
+console.log("sumOutput", sumOutput);
 
 const differenceOutput = x1 - y1;
 
-console.log("differenceOutput", Math.floor(differenceOutput));
+console.log("differenceOutput", differenceOutput);
 
 const productOutput = x1 * y1;
 
-console.log("productOutput", Math.floor(productOutput));
+console.log("productOutput", productOutput);
 
 const quotientOutput = x1 / y1;
 
-console.log("quotientOutput", Math.floor(quotientOutput));
+console.log("quotientOutput", quotientOutput);
 
 const remainderOutput = x1 % y1;
 
-console.log("remainderOutput", Math.floor(remainderOutput));
+console.log("remainderOutput", remainderOutput);
