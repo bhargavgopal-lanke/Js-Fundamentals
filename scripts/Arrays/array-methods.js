2c5278c18d139099e17e795a846d83bf2643f9c8
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-19 13:00:28 +0530

    array methods

---
+++ scripts/Arrays/array-methods.js
@@ -0,0 +1,21 @@
+const arr = [14, 20, 32, 16, 71];
+
+// array methods to manipulate the current array
+
+// push method adds the item to end of an array
+arr.push(99);
+
+// pop method remove the item at end of an array
+arr.pop();
+
+// unshift method adds the item at begining of an array
+arr.unshift(22);
+
+// shift method adds remove item at begining of an array
+arr.shift();
+
+// reverse the current array
+
+arr.reverse();
+
+console.log(arr);
