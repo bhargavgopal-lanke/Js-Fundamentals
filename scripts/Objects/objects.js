7cebbfd5a88bab049f214ec468be2e391d886b59
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-19 16:37:57 +0530

    object destrcturing

---
+++ scripts/Objects/objects.js
@@ -0,0 +1,15 @@
+let x;
+
+const obj = {
+  name: " bhargav123",
+  age: 28,
+  isAdmin: true,
+};
+
+// destructuring using . notation
+x = obj.name;
+
+// destructuring using bracket notation
+x = obj["name"];
+
+console.log("obj", x);
