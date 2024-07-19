66589377c3c47bcda97d796b0f1f9f449c649673
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-19 15:50:47 +0530

    challenge 1 done

---
+++ scripts/Arrays/array-challenge.js
@@ -0,0 +1,8 @@
+// ad 6 and 0 in the begining and end of the array
+
+const challengeArray = [1, 2, 3, 4, 5];
+
+challengeArray.push(0);
+challengeArray.unshift(6);
+
+console.log("challengeArray", challengeArray);
