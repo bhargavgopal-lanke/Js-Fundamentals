a5ee8bb8bc0d1112979b43648cf88d496f17ecd0
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-19 16:51:19 +0530

    object destructuring

--- scripts/Objects/objects.js
+++ scripts/Objects/objects.js
@@ -4,6 +4,12 @@ const obj = {
   name: " bhargav123",
   age: 28,
   isAdmin: true,
+  address: {
+    street: "123 main street",
+    city: "Hyderabad",
+    state: "Telangana",
+  },
+  hobbies: ["sports", "reading"],
 };
 
 // destructuring using . notation
@@ -11,5 +17,10 @@ x = obj.name;
 
 // destructuring using bracket notation
 x = obj["name"];
+
+x = obj.address["street"];
+x = obj.address["state"];
+x = obj.hobbies;
+x = obj.hobbies[0];
 
 console.log("obj", x);
