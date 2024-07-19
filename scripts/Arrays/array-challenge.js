a85ae4be1b123c7fb7646e678f29c5d74958d41f
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-19 15:57:42 +0530

    challenge 2 is done

--- scripts/Arrays/array-challenge.js
+++ scripts/Arrays/array-challenge.js
@@ -5,4 +5,17 @@ const challengeArray = [1, 2, 3, 4, 5];
 challengeArray.push(0);
 challengeArray.unshift(6);
 
-console.log("challengeArray", challengeArray);
+console.log("challenge 1 =>", challengeArray);
+
+// combine arr1 and arr2 into a new array arr3
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9];
+
+let arr3;
+
+arr2.splice(0, 1);
+
+arr3 = [...arr1, ...arr2];
+
+console.log("challenge 2 =>", arr3);
