39fa89c0cbf4b58fc0356df7cd3bbe545a3fdd01
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-19 12:37:09 +0530

    accessing items in an array

---
+++ scripts/Arrays/Arrays.js
@@ -0,0 +1,15 @@
+let x;
+
+// array literal
+
+const array = [12, 14, 16, 20, 25];
+
+// Array constructor
+
+const fruits = new Array("apple", "banana", "grape", "orange");
+
+console.log(fruits);
+
+x = array[1];
+
+console.log(x);
