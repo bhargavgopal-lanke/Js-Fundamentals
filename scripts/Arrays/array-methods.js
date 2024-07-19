e8ab6b1538a5534c1124561305739a1d3d69da90
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-19 13:31:48 +0530

    find the index of an element using indexOf method

--- scripts/Arrays/array-methods.js
+++ scripts/Arrays/array-methods.js
@@ -1,21 +1,32 @@
+// In Js the scripts rus from the top to bottom
+
 const arr = [14, 20, 32, 16, 71];
 
 // array methods to manipulate the current array
 
 // push method adds the item to end of an array
-arr.push(99);
+// arr.push(99);
 
-// pop method remove the item at end of an array
-arr.pop();
+// // pop method remove the item at end of an array
+// arr.pop();
 
-// unshift method adds the item at begining of an array
-arr.unshift(22);
+// // unshift method adds the item at begining of an array
+// arr.unshift(22);
 
-// shift method adds remove item at begining of an array
-arr.shift();
+// // shift method adds remove item at begining of an array
+// arr.shift();
 
-// reverse the current array
+// // reverse the current array
 
-arr.reverse();
+// arr.reverse();
 
-console.log(arr);
+// check if an items exists in the array
+// this returns an boolean value
+
+let y = arr.includes(20);
+
+// find the index of an element using indexOf() method
+
+let res = arr.indexOf(71);
+
+console.log("res", res);
