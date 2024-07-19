fcc0ca253f0e2f79a4955a807ce43da4d7d33169
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-19 12:39:36 +0530

    favurite fruit example

--- scripts/Arrays/Arrays.js
+++ scripts/Arrays/Arrays.js
@@ -12,4 +12,12 @@ console.log(fruits);
 
 x = array[1];
 
+// concatinating the arrays
+
+x = array[0] + array[4];
+
+// get the favourite fruit from the array constructor
+
+x = `My favourite fruit is ${fruits[0]}`;
+
 console.log(x);
