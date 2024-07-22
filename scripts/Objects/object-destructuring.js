2b03c861928c444385dee0cc727b7f2e10e76dae
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-22 10:30:30 +0530

    Obj destructuring example

--- scripts/Objects/object-destructuring.js
+++ scripts/Objects/object-destructuring.js
@@ -2,10 +2,11 @@ const firstName = "Bhargav";
 const lastName = "Lanke";
 const age = 28;
 
+// if the value is a variable and key also same then we dont have to add both
 const desObj = {
   firstName,
   lastName,
   age,
 };
 
-console.log("desObj", desObj.age)
+console.log("desObj", desObj.age);
