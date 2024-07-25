5d3e73b17f75b5726c75050117cb45dbf1a250e1
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-25 12:53:02 +0530

    give only the names of the company

--- scripts/Arrow_functions/map.js
+++ scripts/Arrow_functions/map.js
@@ -1,3 +1,53 @@
+const companies1 = [
+  {
+    name: "Compnay One",
+    Category: "Finance",
+    Start: 1981,
+    end: 2004,
+  },
+  {
+    name: "Compnay two",
+    Category: "Retail",
+    Start: 1992,
+    end: 2008,
+  },
+  {
+    name: "Compnay 33",
+    Category: "Auto",
+    Start: 1999,
+    end: 2007,
+  },
+  {
+    name: "Compnay 4",
+    Category: "Retail",
+    Start: 1989,
+    end: 2010,
+  },
+  {
+    name: "Compnay 5",
+    Category: "Technology",
+    Start: 1987,
+    end: 2010,
+  },
+  {
+    name: "Compnay 5",
+    Category: "Auto",
+    Start: 1986,
+    end: 1996,
+  },
+  {
+    name: "Compnay 5",
+    Category: "Technology",
+    Start: 2011,
+    end: 2016,
+  },
+  {
+    name: "Compnay 5",
+    Category: "Retail",
+    Start: 1981,
+    end: 1989,
+  },
+];
 
 const array = [1, 2, 3, 4, 5, 6];
 
@@ -15,3 +65,9 @@ array.forEach((x) => {
 });
 
 console.log("doubleItem", doubleItem);
+
+// filter out the company items from the company array
+
+const companyMap = companies1.map((x) => x.name);
+
+console.log("companyMap", companyMap);
