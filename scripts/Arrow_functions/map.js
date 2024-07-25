6ff5e99600fc782302c20f94f391afedbb033203
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-25 12:50:47 +0530

    double the items ina an array using forEach method

--- scripts/Arrow_functions/map.js
+++ scripts/Arrow_functions/map.js
@@ -1,3 +1,4 @@
+
 const array = [1, 2, 3, 4, 5, 6];
 
 // concat the number using map method
@@ -5,3 +6,12 @@ const array = [1, 2, 3, 4, 5, 6];
 const concatMap = array.map((x) => "Number " + x);
 
 console.log("concatMap", concatMap);
+
+// double the numbers using foreach method
+// below is an acumulator pattern
+const doubleItem = [];
+array.forEach((x) => {
+  doubleItem.push(x * 2);
+});
+
+console.log("doubleItem", doubleItem);
