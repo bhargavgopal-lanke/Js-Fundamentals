8d7568ce7a99d915f995f2d7295a6697a79b90c8
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-25 12:22:06 +0530

    filter out the companies with the name retail

---
+++ scripts/Arrow_functions/arrow.js
@@ -0,0 +1,58 @@
+const companies = [
+  {
+    name: "Compnay One",
+    Category: "Finance",
+    Start: 1981,
+    end: 2004,
+  },
+  {
+    name: "Compnay two",
+    Category: "Retail",
+    Start: 1992,
+    end: 2008,
+  },
+  {
+    name: "Compnay 33",
+    Category: "Auto",
+    Start: 1999,
+    end: 2007,
+  },
+  {
+    name: "Compnay 4",
+    Category: "Retail",
+    Start: 1989,
+    end: 2010,
+  },
+  {
+    name: "Compnay 5",
+    Category: "Technology",
+    Start: 1987,
+    end: 2010,
+  },
+  {
+    name: "Compnay 5",
+    Category: "Auto",
+    Start: 1986,
+    end: 1996,
+  },
+  {
+    name: "Compnay 5",
+    Category: "Technology",
+    Start: 2011,
+    end: 2016,
+  },
+  {
+    name: "Compnay 5",
+    Category: "Retail",
+    Start: 1981,
+    end: 1989,
+  },
+];
+
+// filter out the companies with the name retail
+
+const filterNames = companies.filter(
+  (companies) => companies.Category === "Retail"
+);
+
+console.log("filterNames", filterNames);
