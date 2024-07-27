df9421a9ef36f71611b6c3b1f791c2ebc6b82121
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-27 12:48:39 +0530

    basic if else statement

---
+++ scripts/logical-operators/if.js
@@ -0,0 +1,16 @@
+if (true) {
+  console.log("this is true");
+}
+
+if (false) {
+  console.log("this is NOT true");
+}
+
+const x = 10;
+const y = 5;
+
+if (x > y) {
+  console.log(`${x} is greater than ${y}`);
+} else {
+  console.log(`${x} is not greater than ${y}`);
+}
