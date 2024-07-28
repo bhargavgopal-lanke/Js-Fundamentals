cdfe57d5db22dfe3fd16e54c44462994f5269e65
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-28 22:17:00 +0530

    if else condition.

--- scripts/logical-operators/if.js
+++ scripts/logical-operators/if.js
@@ -14,3 +14,31 @@ if (x > y) {
 } else {
   console.log(`${x} is not greater than ${y}`);
 }
+
+// date example
+
+const d = new Date();
+
+const hour = d.getHours();
+const minutes = d.getMinutes();
+
+console.log("d", hour);
+console.log("d minutes", minutes);
+
+if (hour < 12) {
+  console.log("Good Morning!");
+} else if (hour > 12) {
+  console.log("Good Afternoon");
+} else {
+  console.log("Good Night");
+}
+
+var expect = function (val) {
+  if (val === 5) {
+    return JSON.stringify({ value: true });
+  } else {
+    console.log("error");
+  }
+};
+
+console.log(expect(5));
