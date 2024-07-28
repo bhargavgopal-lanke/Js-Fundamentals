161573a193e46b961a6c3baf966f93cb7afa47bc
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-28 23:03:35 +0530

    calculator example.

---
+++ scripts/Calculator-Challenge/calculator.js
@@ -0,0 +1,29 @@
+function calculator(num1, num2, operator) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+  }
+  return result;
+}
+
+const add = calculator(5, 2, "+");
+const substract = calculator(5, 2, "-");
+const multiply = calculator(5, 2, "*");
+const divide = calculator(5, 2, "/");
+
+console.log("add", add);
+console.log("substract", substract);
+console.log("multiply", multiply);
+console.log("divide", divide);
