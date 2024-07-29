7eed389f230181e589aae4ab2229e2289981a6ca
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-29 15:19:32 +0530

    loops concept comments

---
+++ scripts/Loop-and-hof/loop.js
@@ -0,0 +1,7 @@
+// Initial Expression - Initialises a variable/counter
+// Condition Expression - Condition that the loop will continue to run
+// as long as it is met or until the condition is false.
+// Increment Expression - Epression that will be executed after each iteration of the loop.
+// Usually increments the variable.
+// Statement - Code that will be executed each time the loop is run.
+// To execute a `block` of code use the `{ }` syntax
