c73126d57b1e082ba43473e4ef49dbc9cbffbe2c
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-30 14:46:11 +0530

    looping throuhgh  an array using while loop.

--- scripts/Loop-and-hof/loop.js
+++ scripts/Loop-and-hof/loop.js
@@ -21,8 +21,49 @@
 
 // nested loops
 
-for (let i = 0; i <= 10; i++) {
-  for (let j = 0; j <= 5; j++) {
-    console.log(`${i} * ${j} = ${i * j}`);
-  }
+// for (let i = 0; i <= 10; i++) {
+//   for (let j = 0; j <= 5; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
+
+// loop through an array
+
+// const names = ["bhargav", "Sam", "Sara", "John", "Tim"];
+
+// for (let i = 0; i < names.length; i++) {
+//   if (i === 0) {
+//     console.log(names[i] + " is the best");
+//   } else {
+//     console.log(names[i]);
+//   }
+// }
+
+// continue
+
+// for (let i = 0; i <= 20; i++) {
+//   if (i === 13) {
+//     console.log("remove number 13");
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// while loop
+
+let i = 0;
+
+while (i <= 20) {
+  console.log("While Number " + i);
+  i++;
+}
+
+// loop over arrays using whoile loop
+
+const arr = [10, 20, 30, 40, 50];
+
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
 }
