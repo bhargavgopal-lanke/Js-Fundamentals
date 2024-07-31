d153c1bd6b81de51fc53c749293b56247871f0a6
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-31 12:02:00 +0530

    event numbers with forEach.

--- scripts/Loop-and-hof/hof.js
+++ scripts/Loop-and-hof/hof.js
@@ -3,3 +3,15 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
 console.log("evenNumbers", evenNumbers);
+
+// same with forEach
+
+const evenNumbers1 = [];
+
+const result = evenNumbers.forEach((nums) => {
+  if (nums % 2 === 0) {
+    evenNumbers1.push(nums);
+  }
+});
+
+console.log("evenNumbers1", evenNumbers1);
