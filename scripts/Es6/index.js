a1d426a5276b3925617947fea1a195cf9426e224
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-08-08 10:28:31 +0530

    object destructuring  practice.

---
+++ scripts/Es6/index.js
@@ -0,0 +1,11 @@
+const personalInformation = {
+  firstName: "Bhargav",
+  lastName: "Lanke",
+  city: "Charlotte",
+  State: "Northa Carolina",
+  zipCode: 1774,
+};
+
+const { firstName: fn, lastName: ln } = personalInformation;
+
+console.log(`${fn}, ${ln}`);
